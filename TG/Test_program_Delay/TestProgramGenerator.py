1e8256abaa7903fc937cc72ef1a4b9f8c09e807b
aysteph3 <aysteph3@yahoo.com>
2020-09-18 22:48:15 +0300

    update TDF

--- TG/Test_program_Delay/TestProgramGenerator.py
+++ TG/Test_program_Delay/TestProgramGenerator.py
@@ -137,10 +137,10 @@ op1_template_optimized.ops1_template(parameter, out, result_register, result_add
 pipeline.syscall(out)
 
 #template for psuedo-exhaustive data
-#out.write(";..........data-path test..........;\n")
-#out.write(" lui $%s, %d\n" % (result_address, 1))
-#out.write(" ori $%s, $%s, %d\n\n" % (result_address, result_address, 10000))
-#pseudo_template.make_pseudo_template(parameter,out, result_register)
+out.write(";..........data-path test..........;\n")
+out.write(" lui $%s, %d\n" % (result_address, 1))
+out.write(" ori $%s, $%s, %d\n\n" % (result_address, result_address, 10000))
+pseudo_template.make_pseudo_template(parameter,out, result_register)
 
 #break
 pipeline.breaks(out)
